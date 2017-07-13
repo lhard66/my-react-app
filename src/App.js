4b097e88cc483fb0c8e4fb8814916ee6761b1f40
Hero Liu <lhard@qq.com>
2017-07-13 10:27:39 +0800

    添加jsx基本使用的注释说明

--- src/App.js
+++ src/App.js
@@ -20,8 +20,12 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> Hello world,{formatName(usr)}!yes.
-        </p>        
+          To get started, edit <code>src/App.js</code>
+        </p>
+        {/* jsx的基本使用 */}
+        <p>
+           Hello world,{formatName(usr)}!
+        </p>
       </div>
     );
   }
